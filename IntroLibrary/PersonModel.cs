27dac812bcd907038410c3ec4be40f97b7ab2a2f
Thiago da Costa <thiagodacosta85@outlook.com>
2021-02-25 16:37:49 -0300

    first commit

---
+++ IntroLibrary/PersonModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IntroLibrary
+{
+    public class PersonModel
+    {
+       public string FirstName { get; set; } 
+       public string LastName { get; set; }
+       public int Age { get; set; }
+    }
+}
---
+++ IntroUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel
+            {
+                FirstName = "Thiago",
+                LastName = "da Costa"
+            };
+
+            System.Console.WriteLine($"{ p.FirstName} { p.LastName} is my name.");
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test");
+            System.Console.WriteLine("The end.");
+        }
+    }
+}
